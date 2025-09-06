a4c9f33625dc2c7be363b9fe782a146112181ac3
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2025-09-06 20:22:13 +0700

    [+] update

--- jaspr_falkit/lib/components/seo/meta_tag.dart
+++ jaspr_falkit/lib/components/seo/meta_tag.dart
@@ -1,83 +1,5 @@
 import 'package:jaspr_falkit/lib.dart';
 
-class SeoMetaTag {
-  const SeoMetaTag._internal({
-    //**** Default SEO *****//
-    this.favicon,
-    this.faviconSvg,
-    this.publisher,
-    this.title,
-    this.description,
-    this.keywords,
-    this.author,
-    this.robots,
-    this.url,
-    this.imageUrl,
-    this.canonical,
-    required this.type,
-    this.locale,
-    required this.imageWidth,
-    required this.imageHeight,
-    this.imageAlt,
-    this.video,
-    this.iconUrl,
-    this.color,
-    this.manifest,
-    this.openGraph,
-    this.openGraphArticle,
-    this.imageSrc,
-    this.twitter,
-    this.pinterest,
-    this.apple,
-    this.microsoft,
-    this.schemaWebSite,
-    this.schemaBlog,
-    this.schemaBlogPosting,
-    this.schemaPerson,
-    this.schemaOrganization,
-    this.alternateLanguageUrls,
-    this.breadcrumbItems,
-  });
-
-  factory SeoMetaTag.builder() => SeoMetaTagBuilder().build();
-
-  //**** Default SEO *****//
-  final String? favicon;
-  final String? faviconSvg;
-  final String? publisher;
-  final String? title;
-  final String? description;
-  final List<String>? keywords;
-  final String? author;
-  final String? robots;
-  final String? url;
-  final String? imageUrl;
-  final String? canonical;
-  final OgType type;
-  final String? locale;
-  final String imageWidth;
-  final String imageHeight;
-  final String? imageAlt;
-  final String? video;
-  final String? iconUrl;
-  final String? color;
-  final String? manifest;
-  final String? imageSrc;
-  final DefaultOpenGraphMeta? openGraph;
-  final ArticleOpenGraphMeta? openGraphArticle;
-  final TwitterMeta? twitter;
-  final PinterestMeta? pinterest;
-  final AppleMeta? apple;
-  final MicrosoftMeta? microsoft;
-  final List<AlternateLanguageTag>? alternateLanguageUrls;
-  final List<BreadcrumbItem>? breadcrumbItems;
-  final WebSiteSchemaData? schemaWebSite;
-  final BlogSchema? schemaBlog;
-  final BlogPostingSchema? schemaBlogPosting;
-  final PersonSchema? schemaPerson;
-  final OrganizationSchema? schemaOrganization;
-}
-
 class SeoMetaTagBuilder {
   SeoMetaTagBuilder();
 
@@ -117,8 +39,8 @@ class SeoMetaTagBuilder {
   List<BreadcrumbItem>? breadcrumbItems;
   SeoMetaTagBuilder? builder;
 
-  SeoMetaTag build() {
-    return SeoMetaTag._internal(
+  SeoMetaTags build() {
+    return SeoMetaTags(
       favicon: builder?.favicon ?? favicon,
       faviconSvg: builder?.faviconSvg ?? faviconSvg,
       publisher: builder?.publisher ?? publisher,
@@ -160,181 +82,257 @@ class SeoMetaTagBuilder {
 }
 
 class SeoMetaTags extends Fragment {
-  SeoMetaTags(SeoMetaTag tag)
-    : super(
-        children: [
-          DefaultMeta(
-            title: tag.title,
-            description: tag.description,
-            keywords: tag.keywords,
-            author: tag.author,
-            robots: tag.robots,
-            publisher: tag.publisher,
-            favicon: tag.favicon,
-            faviconSvg: tag.faviconSvg,
-            canonical: tag.canonical ?? tag.url,
-            themeColor: tag.color,
-            manifest: tag.manifest,
-            imageSrc: tag.imageSrc ?? tag.imageUrl,
-          ),
-          DefaultOpenGraphMeta(
-            title: tag.openGraph?.title ?? tag.title,
-            type: tag.openGraph?.type ?? tag.type,
-            url: tag.openGraph?.url ?? tag.url,
-            imageUrl: tag.openGraph?.imageUrl ?? tag.imageUrl,
-            description: tag.openGraph?.description ?? tag.description,
-            siteName: tag.openGraph?.siteName ?? tag.title,
-            locale: tag.openGraph?.locale ?? tag.locale,
-            imageAlt: tag.openGraph?.imageAlt ?? tag.imageAlt,
-            video: tag.openGraph?.video ?? tag.video,
-            imageHeight: tag.openGraph?.imageHeight ?? tag.imageHeight,
-            imageWidth: tag.openGraph?.imageWidth ?? tag.imageWidth,
-          ),
-          if (tag.type == OgType.article ||
-              tag.openGraph?.type == OgType.article)
-            ArticleOpenGraphMeta(
-              author: tag.openGraphArticle?.author ?? tag.author,
-              section: tag.openGraphArticle?.section,
-              tags: tag.openGraphArticle?.tags,
-              publishedTime: tag.openGraphArticle?.publishedTime,
-              modifiedTime: tag.openGraphArticle?.modifiedTime,
-            ),
-          TwitterMeta(
-            site: tag.twitter?.site,
-            card: tag.twitter?.card,
-            creator: tag.twitter?.creator,
-            title: tag.twitter?.title ?? tag.title,
-            description: tag.twitter?.description ?? tag.description,
-            image: tag.twitter?.image ?? tag.imageUrl,
-            imageAlt: tag.twitter?.imageAlt ?? tag.imageAlt,
-          ),
-          PinterestMeta(
-            pinterestRichPin: tag.pinterest?.pinterestRichPin,
-            author: tag.pinterest?.author ?? tag.author,
-          ),
-          AppleMeta(
-            title: tag.apple?.title ?? tag.title,
-            iconUrl: tag.apple?.iconUrl ?? tag.iconUrl,
-            color: tag.apple?.color ?? tag.color,
-            capable: tag.apple?.capable,
-            fullscreen: tag.apple?.fullscreen,
-          ),
-          MicrosoftMeta(
-            tileColor: tag.microsoft?.tileColor ?? tag.color,
-            tileImageUrl: tag.microsoft?.tileImageUrl ?? tag.iconUrl,
-          ),
-          if (tag.alternateLanguageUrls != null &&
-              (tag.alternateLanguageUrls?.isNotEmpty ?? false))
-            AlternateLanguageMeta(tag.alternateLanguageUrls!),
-          SchemaGroup(
-            id: 'schema-group',
-            schemas: [
-              if (tag.schemaBlog != null) tag.schemaBlog!,
-              if (tag.type == OgType.article ||
-                  tag.openGraph?.type == OgType.article) ...[
-                BlogPostingSchema(
-                  headline: tag.schemaBlogPosting?.headline ?? tag.title,
-                  description:
-                      tag.schemaBlogPosting?.description ?? tag.description,
-                  url: tag.schemaBlogPosting?.url ?? tag.url,
-                  datePublished: tag.schemaBlogPosting?.datePublished,
-                  dateModified: tag.schemaBlogPosting?.dateModified,
-                  author:
-                      tag.schemaBlogPosting?.author ??
-                      tag.author?.toSchemaDataType(),
-                  publisher:
-                      tag.schemaBlogPosting?.publisher ??
-                      tag.publisher?.toSchemaDataType(),
-                  image:
-                      tag.schemaBlogPosting?.image ??
-                      tag.imageUrl?.toSchemaDataType(),
-                  keywords: tag.schemaBlogPosting?.keywords ?? tag.keywords,
-                  articleSection: tag.schemaBlogPosting?.articleSection,
-                  wordCount: tag.schemaBlogPosting?.wordCount,
-                  timeRequired: tag.schemaBlogPosting?.timeRequired,
-                  inLanguage: tag.schemaBlogPosting?.inLanguage ?? tag.locale,
-                  articleBody: tag.schemaBlogPosting?.articleBody,
-                  additionalProperties:
-                      tag.schemaBlogPosting?.additionalProperties,
-                ),
-              ],
-              if (tag.type == OgType.website ||
-                  tag.openGraph?.type == OgType.website)
-                WebSiteSchema(
-                  name: tag.schemaWebSite?.name ?? tag.title,
-                  description:
-                      tag.schemaWebSite?.description ?? tag.description,
-                  url: tag.schemaWebSite?.url ?? tag.url,
-                  inLanguage: tag.schemaWebSite?.inLanguage ?? tag.locale,
-                  datePublished: tag.schemaWebSite?.datePublished,
-                  dateModified: tag.schemaWebSite?.dateModified,
-                  author:
-                      tag.schemaWebSite?.author ??
-                      tag.author?.toSchemaDataType(),
-                  publisher:
-                      tag.schemaWebSite?.publisher ??
-                      tag.publisher?.toSchemaDataType(),
-                  keywords: tag.schemaWebSite?.keywords ?? tag.keywords,
-                  image:
-                      tag.schemaWebSite?.image ??
-                      tag.imageUrl?.toSchemaDataType(),
-                  mainEntity: tag.schemaWebSite?.mainEntity,
-                  additionalProperties: tag.schemaWebSite?.additionalProperties,
-                ),
-              if (tag.schemaPerson != null) tag.schemaPerson!,
-              if (tag.schemaOrganization != null) tag.schemaOrganization!,
-              if (tag.breadcrumbItems != null &&
-                  (tag.breadcrumbItems?.isNotEmpty ?? false))
-                BreadcrumbListSchema(items: tag.breadcrumbItems!),
-            ],
-          ),
-        ],
-      );
+  SeoMetaTags({
+    //**** Default SEO *****//
+    this.favicon,
+    this.faviconSvg,
+    this.publisher,
+    this.title,
+    this.description,
+    this.keywords,
+    this.author,
+    this.robots,
+    this.url,
+    this.imageUrl,
+    this.canonical,
+    required this.type,
+    this.locale,
+    required this.imageWidth,
+    required this.imageHeight,
+    this.imageAlt,
+    this.video,
+    this.iconUrl,
+    this.color,
+    this.manifest,
+    this.openGraph,
+    this.openGraphArticle,
+    this.imageSrc,
+    this.twitter,
+    this.pinterest,
+    this.apple,
+    this.microsoft,
+    this.schemaWebSite,
+    this.schemaBlog,
+    this.schemaBlogPosting,
+    this.schemaPerson,
+    this.schemaOrganization,
+    this.alternateLanguageUrls,
+    this.breadcrumbItems,
+  }) : super(
+         children: [
+           DefaultMeta(
+             title: title,
+             description: description,
+             keywords: keywords,
+             author: author,
+             robots: robots,
+             publisher: publisher,
+             favicon: favicon,
+             faviconSvg: faviconSvg,
+             canonical: canonical ?? url,
+             themeColor: color,
+             manifest: manifest,
+             imageSrc: imageSrc ?? imageUrl,
+           ),
+           DefaultOpenGraphMeta(
+             title: openGraph?.title ?? title,
+             type: openGraph?.type ?? type,
+             url: openGraph?.url ?? url,
+             imageUrl: openGraph?.imageUrl ?? imageUrl,
+             description: openGraph?.description ?? description,
+             siteName: openGraph?.siteName ?? title,
+             locale: openGraph?.locale ?? locale,
+             imageAlt: openGraph?.imageAlt ?? imageAlt,
+             video: openGraph?.video ?? video,
+             imageHeight: openGraph?.imageHeight ?? imageHeight,
+             imageWidth: openGraph?.imageWidth ?? imageWidth,
+           ),
+           if (type == OgType.article || openGraph?.type == OgType.article)
+             ArticleOpenGraphMeta(
+               author: openGraphArticle?.author ?? author,
+               section: openGraphArticle?.section,
+               tags: openGraphArticle?.tags,
+               publishedTime: openGraphArticle?.publishedTime,
+               modifiedTime: openGraphArticle?.modifiedTime,
+             ),
+           TwitterMeta(
+             site: twitter?.site,
+             card: twitter?.card,
+             creator: twitter?.creator,
+             title: twitter?.title ?? title,
+             description: twitter?.description ?? description,
+             image: twitter?.image ?? imageUrl,
+             imageAlt: twitter?.imageAlt ?? imageAlt,
+           ),
+           PinterestMeta(
+             pinterestRichPin: pinterest?.pinterestRichPin,
+             author: pinterest?.author ?? author,
+           ),
+           AppleMeta(
+             title: apple?.title ?? title,
+             iconUrl: apple?.iconUrl ?? iconUrl,
+             color: apple?.color ?? color,
+             capable: apple?.capable,
+             fullscreen: apple?.fullscreen,
+           ),
+           MicrosoftMeta(
+             tileColor: microsoft?.tileColor ?? color,
+             tileImageUrl: microsoft?.tileImageUrl ?? iconUrl,
+           ),
+           if (alternateLanguageUrls != null &&
+               (alternateLanguageUrls?.isNotEmpty ?? false))
+             AlternateLanguageMeta(alternateLanguageUrls!),
+           SchemaGroup(
+             id: 'schema-group',
+             schemas: [
+               if (schemaBlog != null) schemaBlog!,
+               if (type == OgType.article ||
+                   openGraph?.type == OgType.article) ...[
+                 BlogPostingSchema(
+                   headline: schemaBlogPosting?.headline ?? title,
+                   description: schemaBlogPosting?.description ?? description,
+                   url: schemaBlogPosting?.url ?? url,
+                   datePublished: schemaBlogPosting?.datePublished,
+                   dateModified: schemaBlogPosting?.dateModified,
+                   author:
+                       schemaBlogPosting?.author ?? author?.toSchemaDataType(),
+                   publisher:
+                       schemaBlogPosting?.publisher ??
+                       publisher?.toSchemaDataType(),
+                   image:
+                       schemaBlogPosting?.image ?? imageUrl?.toSchemaDataType(),
+                   keywords: schemaBlogPosting?.keywords ?? keywords,
+                   articleSection: schemaBlogPosting?.articleSection,
+                   wordCount: schemaBlogPosting?.wordCount,
+                   timeRequired: schemaBlogPosting?.timeRequired,
+                   inLanguage: schemaBlogPosting?.inLanguage ?? locale,
+                   articleBody: schemaBlogPosting?.articleBody,
+                   additionalProperties:
+                       schemaBlogPosting?.additionalProperties,
+                 ),
+               ],
+               if (type == OgType.website || openGraph?.type == OgType.website)
+                 WebSiteSchema(
+                   name: schemaWebSite?.name ?? title,
+                   description: schemaWebSite?.description ?? description,
+                   url: schemaWebSite?.url ?? url,
+                   inLanguage: schemaWebSite?.inLanguage ?? locale,
+                   datePublished: schemaWebSite?.datePublished,
+                   dateModified: schemaWebSite?.dateModified,
+                   author: schemaWebSite?.author ?? author?.toSchemaDataType(),
+                   publisher:
+                       schemaWebSite?.publisher ??
+                       publisher?.toSchemaDataType(),
+                   keywords: schemaWebSite?.keywords ?? keywords,
+                   image: schemaWebSite?.image ?? imageUrl?.toSchemaDataType(),
+                   mainEntity: schemaWebSite?.mainEntity,
+                   additionalProperties: schemaWebSite?.additionalProperties,
+                 ),
+               if (schemaPerson != null) schemaPerson!,
+               if (schemaOrganization != null) schemaOrganization!,
+               if (breadcrumbItems != null &&
+                   (breadcrumbItems?.isNotEmpty ?? false))
+                 BreadcrumbListSchema(items: breadcrumbItems!),
+             ],
+           ),
+         ],
+       );
+
+  final String? favicon;
+  final String? faviconSvg;
+  final String? publisher;
+  final String? title;
+  final String? description;
+  final List<String>? keywords;
+  final String? author;
+  final String? robots;
+  final String? url;
+  final String? imageUrl;
+  final String? canonical;
+  final OgType type;
+  final String? locale;
+  final String imageWidth;
+  final String imageHeight;
+  final String? imageAlt;
+  final String? video;
+  final String? iconUrl;
+  final String? color;
+  final String? manifest;
+  final String? imageSrc;
+  final DefaultOpenGraphMeta? openGraph;
+  final ArticleOpenGraphMeta? openGraphArticle;
+  final TwitterMeta? twitter;
+  final PinterestMeta? pinterest;
+  final AppleMeta? apple;
+  final MicrosoftMeta? microsoft;
+  final List<AlternateLanguageTag>? alternateLanguageUrls;
+  final List<BreadcrumbItem>? breadcrumbItems;
+  final WebSiteSchemaData? schemaWebSite;
+  final BlogSchema? schemaBlog;
+  final BlogPostingSchema? schemaBlogPosting;
+  final PersonSchema? schemaPerson;
+  final OrganizationSchema? schemaOrganization;
 }
 
-class Meta extends DomComponent {
-  Meta({
-    super.tag = 'meta',
-    String? id,
-    String? name,
-    String? property,
-    String? content,
-    bool unique = false,
-    super.children,
-  }) : super(
-         attributes: {
-           if (unique) 'id': 'meta_$name$property$id'.hashStr(length: 5),
-           if (id.isNotNullOrBlank && unique == false) 'id': id!,
-           if (name.isNotNullOrBlank) 'name': name!,
-           if (property.isNotNullOrBlank) 'property': property!,
-           if (content.isNotNullOrBlank) 'content': content!,
-         },
-       );
+class Meta extends StatelessComponent {
+  const Meta({
+    this.id,
+    this.name,
+    this.property,
+    this.content,
+    this.unique = false,
+  });
+
+  final String? id;
+  final String? name;
+  final String? property;
+  final String? content;
+  final bool unique;
+
+  @override
+  Iterable<Component> build(BuildContext context) => [
+    meta(
+      id: unique ? 'meta_$id$name$property'.hashStr(length: 5) : id,
+      name: name,
+      content: content,
+      attributes: {
+        if (property.isNotNullOrBlank) 'property': property!,
+      },
+    ),
+  ];
 }
 
-class LinkHeader extends DomComponent {
-  LinkHeader({
-    super.key,
-    super.classes,
-    super.styles,
-    required String href,
-    super.events,
-    String? id,
-    String? rel,
-    String? type,
-    String? as,
-    Map<String, String>? attributes,
-    bool unique = false,
-  }) : super(
-         tag: 'link',
-         attributes: {
-           ...?attributes,
-           if (unique) 'id': 'link_$rel$type$as'.hashStr(length: 5),
-           if (id.isNotNullOrBlank && unique == false) 'id': id!,
-           'href': href,
-           if (rel != null) 'rel': rel,
-           if (type != null) 'type': type,
-           if (as != null) 'as': as,
-         },
-       );
+class LinkHeader extends StatelessComponent {
+  const LinkHeader({
+    required this.href,
+    this.id,
+    this.rel,
+    this.type,
+    this.as,
+    this.attributes,
+    this.events,
+    this.unique = true,
+  });
+
+  final String href;
+  final String? id;
+  final String? rel;
+  final String? type;
+  final String? as;
+  final Map<String, String>? attributes;
+  final Map<String, EventCallback>? events;
+  final bool unique;
+
+  @override
+  Iterable<Component> build(BuildContext context) => [
+    link(
+      href: href,
+      id: unique ? 'link_$id$rel$type$as$attributes'.hashStr(length: 5) : id,
+      rel: rel,
+      type: type,
+      as: as,
+      attributes: attributes,
+      events: events,
+    ),
+  ];
 }
