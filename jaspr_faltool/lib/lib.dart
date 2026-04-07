87e4212ebdcbf47cab2dbce152c97a10ef4bb6b8
Nonthawit Doungsodsri <nonthawit.kub@gmail.com>
2026-04-07 12:14:35 +0700

    fix(faltool): drop Unit/option hides removed in jaspr 0.22

--- jaspr_faltool/lib/lib.dart
+++ jaspr_faltool/lib/lib.dart
@@ -2,6 +2,6 @@ export 'dart:async';
 export 'dart:convert';
 
 export 'package:ansicolor/ansicolor.dart';
-export 'package:jaspr/jaspr.dart' hide IterableFilter, Unit, option;
+export 'package:jaspr/jaspr.dart' hide IterableFilter;
 
 export 'jaspr_faltool.dart';
